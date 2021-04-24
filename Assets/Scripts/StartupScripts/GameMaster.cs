8d8a3f25c2b39640d26ae8e550660eee58de77b0
DESKTOP-KS1OKH1\andre <andrew@grcmc.org>
2021-04-24 19:05:20 -0400

    spawning a tree

--- Assets/Scripts/StartupScripts/GameMaster.cs
+++ Assets/Scripts/StartupScripts/GameMaster.cs
@@ -9,6 +9,8 @@ public class GameMaster : MonoBehaviour
     [SerializeField]
     private GameObject[] _dirtPrefabs;
     [SerializeField]
+    private GameObject _treePreFab;
+    [SerializeField]
     private GameObject _seedPrefab;
     [SerializeField]
     private int _dirtWidth;
@@ -20,6 +22,7 @@ public class GameMaster : MonoBehaviour
     private float _seedYPos;
 
     public GameObject[,] TerrainTiles { get; set; }
+    public GameObject[,] TreeTiles { get; set; }
     public GameObject Seed { get; set; }
 
     void Start()
@@ -31,11 +34,24 @@ public class GameMaster : MonoBehaviour
 
         // spawn de seed
         Seed = Spawner.SpawnPrefab(_seedPrefab, _seedXPos, _seedYPos);
+
+        spawnTree(15);
     }
 
     private void ValidatePrefabs()
     {
         if(_dirtPrefabs.Any(p => p.GetComponent<BaseTile>() == null))
             throw new MissingComponentException("Prefab is missing BaseTile component");
+    }
+
+    private void spawnTree(int nutrientScore)
+    {
+        int seedYPos = (int) Math.Floor(_seedYPos);
+        int i = 0;
+        while(i < nutrientScore)
+        {
+            Spawner.SpawnPrefab(_treePreFab, _seedXPos, seedYPos + i);
+            i++;
+        }
     }
 }
