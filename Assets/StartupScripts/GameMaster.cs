e2e9ffc5c952700d1e0a0de2ababbe70e0c01684
deruitda <deruitda@mail.gvsu.edu>
2021-04-24 02:12:59 -0400

    Provide game size through class fields

--- Assets/StartupScripts/GameMaster.cs
+++ Assets/StartupScripts/GameMaster.cs
@@ -6,10 +6,14 @@ public class GameMaster : MonoBehaviour
 {
     [SerializeField]
     private GameObject _dirtPrefab;
+    [SerializeField]
+    private int _dirtWidth;
+    [SerializeField]
+    private int _dirtHeight;
     // Start is called before the first frame update
     void Start()
     {
         // spawn de dirt
-        Spawner.SpawnRectangle(_dirtPrefab, 10, 20);
+        Spawner.SpawnRectangle(_dirtPrefab, _dirtWidth, _dirtHeight);
     }
 }
