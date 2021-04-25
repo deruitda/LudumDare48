f360a9df3bebcc55db0c1365b7a1c8e3d13b5343
DESKTOP-KS1OKH1\andre <andrew@grcmc.org>
2021-04-24 20:23:13 -0400

    creating tree tree spawner and branch

---
+++ Assets/Scripts/TreeScripts/Tree.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.TreeScripts
+{
+    public class Tree
+    {
+        private GameObject _treePreFab;
+        private float _seedXPos;
+        private float _seedYPos;
+        private float height;
+        public Tree(GameObject treePreFab, float seedXPos = 0, float seedYPos = 0)
+        {
+            _treePreFab = treePreFab;
+            _seedXPos = seedXPos;
+            _seedYPos = seedYPos;
+        }
+
+        public void grow()
+        {
+            height++;
+            Spawner.SpawnPrefab(_treePreFab, _seedXPos, _seedYPos + height);
+        }
+    }
+}
--- Assets/Scripts/TreeScripts/TreeSpawner.cs
+++ Assets/Scripts/TreeScripts/TreeSpawner.cs
@@ -2,30 +2,24 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class TreeSpawner
 {
-    private GameObject _treePreFab;
-
     private int _nutrientScore;
-    private float _seedXPos;
-    private float _seedYPos;
+    private Assets.Scripts.TreeScripts.Tree _tree;
 
     public TreeSpawner(GameObject treePreFab, float seedXPos = 0, float seedYPos = 0)
     {
-        this._treePreFab = treePreFab;
-        this._seedXPos = seedXPos;
-        this._seedYPos = seedYPos;
+
+        _tree = new Assets.Scripts.TreeScripts.Tree(treePreFab, seedXPos, seedYPos);
     }
 
     public void SpawnTree(int nutrientScore)
     {
 
-        int seedYPos = (int)Math.Floor(_seedYPos);
         int i = 0;
         while (i < nutrientScore)
         {
-            Spawner.SpawnPrefab(_treePreFab, _seedXPos, seedYPos + i);
+            _tree.grow();
             i++;
         }
     }
