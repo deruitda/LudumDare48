fca637978dfc4d9f83b86ad367531696af4c7eee
amichie1 <34970861+amichie1@users.noreply.github.com>
2021-04-26 19:40:10 -0400

    removing start tree

--- Assets/Scripts/StartupScripts/GameMaster.cs
+++ Assets/Scripts/StartupScripts/GameMaster.cs
@@ -114,25 +114,7 @@ public class GameMaster : MonoBehaviour
         // spawn de seed
         Seed = Spawner.SpawnPrefab(_seedPrefab, _seedXPos, _seedYPos);
 
-        TreeConfig treeConfig = new TreeConfig
-        {
-            treePreFab = _treePreFab,
-            branchPreFab = _treePreFab,
-            leavesPreFab = _leavePreFab,
-            seedXPos = _seedXPos,
-            seedYPos = _seedYPos,
-            chanceOfCreatingABranch = 0.1,
-            chanceOfBranchGrowing = 0.5,
-            minSizeOfTreeBeforeForking = 15,
-            minSizeOfBranchBeforeFork = 12,
-            chanceOfCreatingAFork = 0.4,
-            percentageOfTreeHasLeaves = 0.3,
-            startingWater = StartingWater,
-            maxAmountOfForks = 1000
-        };
         Destroy(Seed);
-        TreeSpawner ts = new TreeSpawner(treeConfig);
-        ts.SpawnTree(20);
     }
 
     // TODO Refactor this method so we can build the neighbor graph during tile instantiation
